8569ba53047a9a9da14a7a2f8ffdd20ee201485e
xwlim1611 <55648013+xwlim1611@users.noreply.github.com>
2020-03-13 19:41:28 +0800

    Add files via upload

---
+++ 5) allEvenDigits.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+int allEvenDigits1(int num);
+void allEvenDigits2(int num, int *result);
+int main()
+{
+    int number, p=999, result=999;
+
+    printf("Enter a number: \n");
+    scanf("%d", &number);
+    p = allEvenDigits1(number);
+    if (p == 1)
+        printf("allEvenDigits1(): yes\n");
+    else if (p == 0)
+        printf("allEvenDigits1(): no\n");
+    else
+        printf("allEvenDigits1(): error\n");
+    allEvenDigits2(number, &result);
+    if (result == 1)
+        printf("allEvenDigits2(): yes\n");
+    else if (result == 0)
+        printf("allEvenDigits2(): no\n");
+    else
+        printf("allEvenDigits2(): error\n");
+    return 0;
+}
+int allEvenDigits1(int num)
+{
+    int counter = 0;
+    int div = 0;
+    while (num > 0)
+    {
+        div = num % 10;
+        if (div % 2 != 0)
+        {
+            counter = 0;
+            return counter;
+        }
+        else
+        {
+            counter = 1;
+        }
+        num /= 10;
+    }
+    return counter;
+}
+void allEvenDigits2(int num, int *result)
+{
+    int counter = 0;
+    int div = 0;
+    while (num > 0)
+    {
+        div = num % 10;
+        if (div % 2 != 0)
+        {
+            counter = 0;
+            *result = counter;
+        }
+        else
+        {
+            counter = 1;
+        }
+        num /= 10;
+    }
+     *result = counter;
+}
