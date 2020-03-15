a97130f5dc77f040cf44e1ca4fae4e621fd7dfd1
xwlim1611 <55648013+xwlim1611@users.noreply.github.com>
2020-03-15 18:51:49 +0800

    Add files via upload

--- Assignments/Week 3/7) power.c
+++ Assignments/Week 3/7) power.c
@@ -3,64 +3,56 @@ float power1(float num, int p);
 void power2(float num, int p, float *result);
 int main()
 {
-    int power;
-    float number, result=-1;
+ int power;
+ float number, result=-1;
 
-    printf("Enter the number and power: \n");
-    scanf("%f %d", &number, &power);
-    printf("power1(): %.2f\n", power1(number, power));
-    power2(number,power,&result);
-    printf("power2(): %.2f\n", result);
-    return 0;
+ printf("Enter the number and power: \n");
+ scanf("%f %d", &number, &power);
+ printf("power1(): %.2f\n", power1(number, power));
+ power2(number,power,&result);
+ printf("power2(): %.2f\n", result);
+ return 0;
 }
 float power1(float num, int p)
 {
-    float finalvalue = num;
-    if (p > 0)
-    {
-        for (p; p > 1; p--)
-        {
-            finalvalue *= num;
-        }
-        return finalvalue;
-    }
-    else if (p < 0)
-    {
-        for (p; p < 1; p++)
-        {
-            finalvalue /= num;
-        }
-        return finalvalue;
-    }
-    else if (p == 0)
-    {
-        finalvalue = 1;
-        return finalvalue;
-    }
-
+ /* Write your code here */
+ int i;
+ float total = 1;
+ if (p > 0)
+ {
+     for(i=0; i<p; i++)
+         total *= num;
+     return total;
+ }
+ else if (p < 0)
+ {
+    for (i=0; i>p; i--)
+        total *= (1/num);
+    return total;
+ }
+ else if (p == 0)
+    return 1;
 }
 void power2(float num, int p, float *result)
 {
-    float finalvalue = num;
-    if (p > 0)
+ /* Write your code here */
+ int i;
+ float total;
+ total = 1;
+ if (p > 0)
+ {
+     for(i=0; i<p; i++)
+         total *= num;
+     *result = total;
+ }
+ else if (p < 0)
+ {
+    for (i=0; i>p; i--)
     {
-        for (p; p > 1; p--)
-        {
-            finalvalue *= num;
-        }
-        *result = finalvalue;
+        total *= (1/num);
     }
-    else if (p < 0)
-    {
-        for (p; p < 1; p++)
-        {
-            finalvalue /= num;
-        }
-        *result = finalvalue;
-    }
-    else if (p == 0)
-    {
-        finalvalue = 1;
-        *result = finalvalue;
-    }
+    *result = total;
+ }
+ else if (p == 0)
+    *result = 1;
 }
