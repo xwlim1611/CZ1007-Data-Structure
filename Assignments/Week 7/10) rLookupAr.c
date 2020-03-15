61f7a310654af61dfd56f410fd1880cdc01cce02
xwlim1611 <55648013+xwlim1611@users.noreply.github.com>
2020-03-16 02:38:23 +0800

    Add files via upload

---
+++ Assignments/Week 7/10) rLookupAr.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int rLookupAr(int array[], int size, int target);
+int main()
+{
+ int numArray[80];
+ int target, i, size;
+ int result=-999;
+ printf("Enter array size: \n");
+ scanf("%d", &size);
+ printf("Enter %d numbers: \n", size);
+ for (i=0; i < size; i++)
+ scanf("%d", &numArray[i]);
+ printf("Enter the target number: \n");
+ scanf("%d", &target);
+ result = rLookupAr(numArray, size, target);
+ printf("rLookupAr(): %d", result);
+ return 0;
+}
+int rLookupAr(int array[], int size, int target)
+{
+ /* Write your code here */
+ if (size >= 0)
+ {
+     if(array[size] == target)
+     {
+         return size;
+     }
+     else
+     {
+         rLookupAr(array, size-1, target);
+     }
+ }
+ else
+    return -1;
+}
