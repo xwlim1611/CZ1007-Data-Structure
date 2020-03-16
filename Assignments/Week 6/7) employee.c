45806b4abec9c692a68bd48fb63b9b3bd8471827
xwlim1611 <55648013+xwlim1611@users.noreply.github.com>
2020-03-16 15:44:52 +0800

    Update 7) employee.c

--- Assignments/Week 6/7) employee.c
+++ Assignments/Week 6/7) employee.c
@@ -82,7 +82,7 @@ int readin(Employee *emp)
     {
         printf("Enter name:\n");
         scanf("\n");
-        gets(emp->name);
+        scanf("%s", &(emp->name));
         strcpy(temp,emp->name);
         if(temp[0]=='#')
             break;
