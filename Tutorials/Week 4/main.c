a3c2fdb947f406d8b40cd28f415efcf1d0becaac
xwlim1611 <55648013+xwlim1611@users.noreply.github.com>
2020-03-16 16:06:00 +0800

    Add files via upload

---
+++ Tutorials/Week 4/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+void printReverse1(int ar[], int size);
+void printReverse2(int ar[], int size);
+void reverseAr1D(int ar[], int size);
+int main()
+{
+    int ar[80];
+    int size, i;
+
+    printf("Enter array size: \n");
+    scanf("%d", &size);
+    printf("Enter %d data: \n", size);
+    for (i=0; i <= size-1; i++)
+        scanf("%d", &ar[i]);
+    printReverse1(ar, size);
+    printReverse2(ar, size);
+    reverseAr1D(ar, size);
+    printf("reverseAr1D(): ");
+    if (size > 0) {
+        for (i=0; i<size; i++)
+            printf("%d ", ar[i]);
+    }
+    return 0;
+}
+void printReverse1(int ar[], int size)
+{
+    int i;
+    printf("printReverse1(): ");
+    if (size > 0)
+    {
+        for (i = size-1; i >= 0; i--)
+        {
+            printf("%d ", ar[i]);
+        }
+    }
+    printf("\n");
+}
+void printReverse2(int ar[], int size)
+{
+    int i;
+    printf("printReverse2(): ");
+    if (size > 0)
+    {
+        for (i = size-1; i >= 0; i--)
+        {
+            printf("%d ", *(ar+i));
+        }
+    }
+    printf("\n");
+}
+void reverseAr1D(int ar[ ], int size)
+{
+    int i, number;
+    if (size > 0)
+    {
+        for (i=0; i < size/2; i++)
+        {
+            number = ar[i];
+            ar[i] = ar[size-i-1];
+            ar [size-i-1] = number;
+        }
+    }
+}
