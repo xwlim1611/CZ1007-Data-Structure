d8c89f3bececa24adb12856c35113fe98062ec50
xwlim1611 <55648013+xwlim1611@users.noreply.github.com>
2020-03-16 16:16:58 +0800

    Add files via upload

---
+++ Lab/Week 7/2) rDigitPos.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int rDigitPos1(int num, int digit);
+void rDigitPos2(int num, int digit, int *pos);
+int main()
+{
+    int number, digit, result=0;
+
+    printf("Enter the number: \n");
+    scanf("%d", &number);
+    printf("Enter the digit: \n");
+    scanf("%d", &digit);
+    printf("rDigitPos1(): %d\n", rDigitPos1(number, digit));
+    rDigitPos2(number, digit, &result);
+    printf("rDigitPos2(): %d\n", result);
+    return 0;
+}
+int rDigitPos1(int num, int digit)
+{
+    int posi;
+    if (num%10 == digit)
+        return 1;
+    else if (num < 10)
+        return 0;
+    else
+    {
+        posi = rDigitPos1(num/10, digit);
+        if (posi > 0)
+            return posi + 1;
+        else
+            return 0;
+    }
+}
+void rDigitPos2(int num, int digit, int *pos)
+{
+    if (num % 10 == digit)
+        *pos = 1;
+    else if (num < 10)
+        *pos = 0;
+    else
+    {
+        rDigitPos2(num/10, digit, pos);
+        if (*pos > 0)
+            *pos += 1;
+        else
+            *pos = 0;
+    }
+}
